8ae835e232a70557166a965447325007fe5cd363
zidanekarim <zkarim7676@gmail.com>
2026-02-15 17:27:39 -0500

    early dependencies

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "NASA asynchronous media cache in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # The Rust Toolchain
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+            })
+            
+            # Other tools you might need
+            pkg-config
+            openssl
+          ];
+
+          shellHook = ''
+            echo "Rust version: $(rustc --version)"
+          '';
+        };
+      }
+    );
+}
